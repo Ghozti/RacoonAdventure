ade9ea5ef98391ae5f9bae09055a09608a20ca7e
Ghozti <ybalderas1005@gmail.com>
2021-02-07 20:17:23 -0500

    Initial commit

---
+++ desktop/src/ghozti/racoonadventure/desktop/DesktopLauncher.java
@@ -0,0 +1,25 @@
+package ghozti.racoonadventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ghozti.racoonadventure.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//sets the application title
+		config.title = "Racoon Adventure";
+		//sets initial app w and h
+		config.width = 1000;
+		config.height = 800;
+		//allows users to resize the application
+		config.resizable = true;
+		//wil pause the game if minized
+		config.pauseWhenMinimized = true;
+
+		//TODO add icon
+		//config.addIcon();
+		new LwjglApplication(new Game(), config);
+	}
+}
